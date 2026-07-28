f7a12efe2d4405739592e79c84a72bb4cca9865e
dheeraj <dheerajsure595@gmail.com>
2026-07-28 16:05:31 +0530

    1st push

---
+++ test.js
@@ -0,0 +1,23 @@
+import request from "supertest";
+import app from "./index.js";
+
+try {
+  const res = await request(app).get("/");
+
+  if (
+    res.status === 200 &&
+    res.body.success === true &&
+    res.body.message === "server is running"
+  ) {
+    console.log("✅ Smoke Test Passed");
+    process.exit(0);
+  } else {
+    console.log("❌ Smoke Test Failed");
+    console.log(res.body);
+    process.exit(1);
+  }
+} catch (err) {
+  console.error("❌ Smoke Test Failed");
+  console.error(err);
+  process.exit(1);
+}
